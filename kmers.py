f2502e5fb4d4fbd836736af02e4946f4f34a89c7
bccl <samuel.young.103@gmail.com>
2018-04-17 10:14:27 -0400

    slidingwindow to the graphing function

--- kmers.py
+++ kmers.py
@@ -1,17 +1,28 @@
 #!/bin/env python 
+import pdb
+
 # this will create the inital call to the kmers value 
 class method:
     def kmers(self,value,seq):
-        q=len(seq)-value
-        for i in range(0,seq):  
+        ls="" 
+        rs=""
+        i=0
+	while i <= value :  
             if (value == i):
-                print (seq[:])
-           print (str(value) + " this is the value and " + seq + "is the sequence." )
-
+                ls= seq[value-i:i]
+            if (value == i):
+                rs = (seq[2*value-i:2*i])
+            i= i + 1
+        side= [ls, rs] 
+        return side
+    def slideingwindow(self,value, side):
+        pdb.set_trace()
+        print ("The new value" + str(value-1) + "I want to see what the two sides look like like" + side + side[2])   
 
 def main():
    meth=method()
    sequence="ATGTACGATC"
-   print(meth.kmers(5,sequence))
+   side=meth.kmers(5,sequence)
+   meth.slideingwindow(5,side)
   
 main() 
