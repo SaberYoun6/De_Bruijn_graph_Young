a6c2d806a34c8d2ac83cbbb9d2becb628450c445
yuriprym <samuel.young.103@gmail.com>
2018-04-24 22:08:40 -0400

    final understand how it works

--- kmers.py
+++ kmers.py
@@ -1,27 +1,35 @@
 #!/bin/env python 
 import pdb
-
+import networkx as nx
+import sys
 # this will create the inital call to the kmers value 
 class method:
-    def kmers(self,value,seq):
-        ls="" 
-        rs=""
-        i=0
-        j=0
-        while (i <= len(seq)):
-            if (value == i):
-                ls= seq[value-i:i]
-            if (value == i):
-                rs = (seq[2*value-i:2*i])
-            i= i + 1
-        side = [ ls , rs ]
-        j=i
-        if (j== len(seq)):
-            self.kmers(value,seq)
-        return side 
-    def slideingwindow(self,value, side):
-        pdb.set_trace()
-        print ("The new value" + str(value-1))   
+   # def kmers(self,value,seq):
+   #     ls="" 
+   #     rs=""
+   #     i=0
+   #     j=0
+   #     while (i <= len(seq)):
+   #         if (value == i):
+   #             ls= seq[value-i:i]
+   #         if (value == i):
+   #             rs = (seq[2*value-i:2*i])
+   #         i= i + 1
+   #     side = [ ls , rs ]
+   #     j=i
+   #     if (j== len(seq)):
+   #         self.kmers(value,seq)
+   #     return side 
+    def de_burjin(self,value,side):
+        edges=[]
+        nodes=set()
+        for i in range(len(side)-value+1):
+            edges.append((side[i:i+value-1],side[i+1:i+value]))
+            nodes.add(side[i:i+value-1])
+            nodes.add(side[i+1:i+value])
+        return nodes,edges
+    def v_de_brujin(self,value,side):
+        nx.algorithms.euler.eulerian_circuit(side,value)
 
 def main():
    meth=method()
@@ -30,12 +38,14 @@ def main():
    #subproblem=int(subproblem)
    #side=meth.kmers(subproblem,sequence)
    #meth.slideingwindow(subproblem,side)
-   sub=input("random graphing frame")
+   sub=input("random graphing frame: ")
    sub=int(sub)
-   neon =input("please input a short genome ")
-   type(neon)
-   nearestkmers=meth.kmers(sub,neon)
-   print(nearestkmers)
+   neon =input("please input a short genome: ")
+   print(type(neon))
+   seq,k=meth.de_burjin(sub,neon)
+   print (seq) 
+   print (k)
+   print (meth.v_de_brujin(seq,k))
   
 main() 
 
